ca2bb34296714db55cf3013c295c7cea705506d9
goodmorning1226 <101035804+goodmorning1226@users.noreply.github.com>
2022-03-13 19:59:47 +0800

    Add files via upload

--- data/data.js
+++ data/data.js
@@ -17,13 +17,13 @@ con3.innerHTML = `
 for(var i = 1; i<6; i++){
     document.getElementById("oa").innerHTML += `礦物${i}*${player[`money${i}`]} `;
 };
-document.getElementById("dbtn1").addEventListener("click", function(){
+function hoverag(num){
     for(var i = 1; i<7; i++){
         document.getElementById(`dbtn${i}`).style.color = "rgba(240, 248, 255, 0.74)";
         document.getElementById(`dbtn${i}`).addEventListener("mouseover", function(){
             this.style.color = "aquamarine";
         });
-        if(i == 1){
+        if(i == num){
             document.getElementById(`dbtn${i}`).addEventListener("mouseout", function(){
                 this.style.color = "red";
             });
@@ -34,122 +34,45 @@ document.getElementById("dbtn1").addEventListener("click", function(){
             });
         };
     };
-    this.style.color = "red";
+    document.getElementById(`dbtn${num}`).style.color = "red";
+};
+document.getElementById("dbtn1").addEventListener("click", function(){
+    hoverag(1);
     con3.innerHTML = `
         <h2>基本資料</h2>
         <p>玩家id:${player["id"]}</p>
         <p>玩家編號:${player["number"]}</p>`;
 });
 document.getElementById("dbtn2").addEventListener("click", function(){
-    for(var i = 1; i<7; i++){
-        document.getElementById(`dbtn${i}`).style.color = "rgba(240, 248, 255, 0.74)";
-        document.getElementById(`dbtn${i}`).addEventListener("mouseover", function(){
-            this.style.color = "aquamarine";
-        });
-        if(i == 2){
-            document.getElementById(`dbtn${i}`).addEventListener("mouseout", function(){
-                this.style.color = "red";
-            });
-        }
-        else{
-            document.getElementById(`dbtn${i}`).addEventListener("mouseout", function(){
-                this.style.color = "rgba(240, 248, 255, 0.74)";
-            });
-        };
-    };
-    this.style.color = "red";
+    hoverag(2);
     con3.innerHTML = `
         <h2>鎬子最高等級</h2>
         <p>最高等級為:${player["picaxelv"]}級</p>
         <img class="picimg" src="../image/picaxe${player["picaxelv"]}.png"/>`;
 });
 document.getElementById("dbtn3").addEventListener("click", function(){
-    for(var i = 1; i<7; i++){
-        document.getElementById(`dbtn${i}`).style.color = "rgba(240, 248, 255, 0.74)";
-        document.getElementById(`dbtn${i}`).addEventListener("mouseover", function(){
-            this.style.color = "aquamarine";
-        });
-        if(i == 3){
-            document.getElementById(`dbtn${i}`).addEventListener("mouseout", function(){
-                this.style.color = "red";
-            });
-        }
-        else{
-            document.getElementById(`dbtn${i}`).addEventListener("mouseout", function(){
-                this.style.color = "rgba(240, 248, 255, 0.74)";
-            });
-        };
-    };
-    this.style.color = "red";
+    hoverag(3);
     con3.innerHTML = `
         <h2>礦洞最高等級</h2>
         <p>最高等級為:${player["cavelv"]}級</p>
         <img class="caveimg" src="../image/cave${player["cavelv"]}.jpg"/>`;
 });
 document.getElementById("dbtn4").addEventListener("click", function(){
-    for(var i = 1; i<7; i++){
-        document.getElementById(`dbtn${i}`).style.color = "rgba(240, 248, 255, 0.74)";
-        document.getElementById(`dbtn${i}`).addEventListener("mouseover", function(){
-            this.style.color = "aquamarine";
-        });
-        if(i == 4){
-            document.getElementById(`dbtn${i}`).addEventListener("mouseout", function(){
-                this.style.color = "red";
-            });
-        }
-        else{
-            document.getElementById(`dbtn${i}`).addEventListener("mouseout", function(){
-                this.style.color = "rgba(240, 248, 255, 0.74)";
-            });
-        };
-    };
-    this.style.color = "red";
+    hoverag(4);
     con3.innerHTML = `
         <h2>目前所使用的鎬子</h2>
         <p>等級為:${player["npicaxelv"]}級</p>
         <img class="picimg" src="../image/picaxe${player["npicaxelv"]}.png"/>`;
 });
 document.getElementById("dbtn5").addEventListener("click", function(){
-    for(var i = 1; i<7; i++){
-        document.getElementById(`dbtn${i}`).style.color = "rgba(240, 248, 255, 0.74)";
-        document.getElementById(`dbtn${i}`).addEventListener("mouseover", function(){
-            this.style.color = "aquamarine";
-        });
-        if(i == 5){
-            document.getElementById(`dbtn${i}`).addEventListener("mouseout", function(){
-                this.style.color = "red";
-            });
-        }
-        else{
-            document.getElementById(`dbtn${i}`).addEventListener("mouseout", function(){
-                this.style.color = "rgba(240, 248, 255, 0.74)";
-            });
-        };
-    };
-    this.style.color = "red";
+    hoverag(5);
     con3.innerHTML = `
         <h2>目前所使用的礦洞</h2>
         <p>等級為:${player["ncavelv"]}級</p>
         <img class="caveimg" src="../image/cave${player["ncavelv"]}.jpg"/>`;
 });
 document.getElementById("dbtn6").addEventListener("click", function(){
-    for(var i = 1; i<7; i++){
-        document.getElementById(`dbtn${i}`).style.color = "rgba(240, 248, 255, 0.74)";
-        document.getElementById(`dbtn${i}`).addEventListener("mouseover", function(){
-            this.style.color = "aquamarine";
-        });
-        if(i == 6){
-            document.getElementById(`dbtn${i}`).addEventListener("mouseout", function(){
-                this.style.color = "red";
-            });
-        }
-        else{
-            document.getElementById(`dbtn${i}`).addEventListener("mouseout", function(){
-                this.style.color = "rgba(240, 248, 255, 0.74)";
-            });
-        };
-    };
-    this.style.color = "red";
+    hoverag(6);
     con3.innerHTML = `<div id="money"><h3 id="moneyh2">所擁有礦物數:</h3></div>`;
     for(var i = 1; i<6; i++){
         document.getElementById("money").innerHTML += `
